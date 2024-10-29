9443d85ed9d424f08410bd2286ec3f912dced73c
atgiuxi <3219576710@qq.com>
2024-10-29 08:53:16 +0800

    try again

---
+++ CPlusPlus/day2024_10_29/test.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "昨天在writing-for-vscode上提交了" << endl;
+    cout << "master分支提交有小绿点" << endl;
+    cout << "今天合并到master分支上，亮的是昨天的的小绿点" << endl;
+
+    return 0;
+}
